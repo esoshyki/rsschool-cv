a2658c9d5f1f920cb34a7f1f8371ec08cbf40d83
esoshyki <esoshyki@yandex.by>
2019-09-13 14:14:44 +0300

    fix: change the visuality of animation

--- animate.js
+++ animate.js
@@ -1,9 +1,16 @@
-const draw = (el, curRect, nextRect) => {
-    const ctx = el.getContext('2d');
-    ctx.strokeStyle="yellowgreen";
-    ctx.lineWidth = 4;
-    ctx.arc(35, 35, 33, 0, 2*Math.PI);
-    ctx.stroke();
+// Initial Data
+const fps = 100; // frames per second
+const arcPart = 2*Math.PI / fps;   // parts of drawing arc in one interval in radians
+const timeDelay = 600; // duration of animation in ms
+const interval = timeDelay / fps; 
+let startAngle = 0;
+let nextAngle = startAngle + arcPart;
+
+const draw = (context, curAng, nextAng) => {
+    context.strokeStyle="yellowgreen";
+    context.lineWidth = 4;
+    context.arc(35, 35, 33, curAng, nextAng);
+    context.stroke();
 }
 
 const remove = (el) => {
@@ -13,13 +20,26 @@ const remove = (el) => {
 
 const handler = (e) => {
     const canv = e.target
-    draw(canv, 0, 2*Math.PI)
-}
+    const ctx = canv.getContext('2d');
+    ctx.beginPath();
+    draw(ctx, startAngle, nextAngle);
+    const timer = () => {
+        remove(canv);
+        startAngle += arcPart; nextAngle += arcPart;
+        draw(ctx, startAngle, nextAngle);
+    }
 
-const remover = (e) => {
-    const canv = e.target
-    remove(canv)
+    const animate = setInterval(timer, interval);
+
+    canv.addEventListener('mouseout', () => {
+        clearInterval(animate);
+        remove(canv);
+        startAngle = 0;
+        nextAngle = startAngle + arcPart;
+    })
+
+    setTimeout(() => {
+        clearInterval(animate)}, timeDelay)
 }
 
 document.querySelectorAll('.icons_animate').forEach(el => {el.addEventListener('mouseover', handler)})
-document.querySelectorAll('.icons_animate').forEach(el => {el.addEventListener('mouseout', remover)})
